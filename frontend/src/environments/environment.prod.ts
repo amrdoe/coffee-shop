d001781bdb18e72b3c44b8cb14beaec87273c483
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-09-22 16:36:53 +0200

    Added missing Auth0 config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
   apiServerUrl: 'https://coffee-shop-backend.onrender.com/', // the running FLASK api server url
+  auth0: {
+    url: 'amrikhudair.eu', // the auth0 domain prefix
+    audience: 'https://coffee-shop.test', // the audience set for the auth0 app
+    clientId: '6foUBjR8Uys0PRrhAEVJ5BF6lltFsYBX', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+  }
 };

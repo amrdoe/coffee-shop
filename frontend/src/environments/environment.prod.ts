fd1f707e2a22d9b813aabc86c90000e43222cd2b
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-09-23 02:41:14 +0200

    Set the callback URL in the production to the live deployment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
     url: 'amrikhudair.eu', // the auth0 domain prefix
     audience: 'https://coffee-shop.test', // the audience set for the auth0 app
     clientId: '6foUBjR8Uys0PRrhAEVJ5BF6lltFsYBX', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+    callbackURL: 'https://coffee-shop-delta.vercel.app', // the base url of the running ionic application.
   }
 };

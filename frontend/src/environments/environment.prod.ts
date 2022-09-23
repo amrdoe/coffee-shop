cd50fac170f9f33d2b19f5316889d929f2c44dae
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-09-23 02:54:50 +0200

    Update Audience

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiServerUrl: 'https://coffee-shop-backend.onrender.com/', // the running FLASK api server url
   auth0: {
     url: 'amrikhudair.eu', // the auth0 domain prefix
-    audience: 'https://coffee-shop.test', // the audience set for the auth0 app
+    audience: 'https://coffee-shop-delta.vercel.app', // the audience set for the auth0 app
     clientId: '6foUBjR8Uys0PRrhAEVJ5BF6lltFsYBX', // the client id generated for the auth0 app
     callbackURL: 'https://coffee-shop-delta.vercel.app', // the base url of the running ionic application.
   }
